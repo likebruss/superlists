ae35f987d87815817aa7007ad8477bab5133aada
Krzysztof Wołowski <krzysztof@mojebudowy.pl>
2017-07-31 17:18:33 +0200

    ch8: start od chapter

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -29,6 +29,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
+
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -66,8 +67,11 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
+        # Satisfied, she goes back to sleep
+
+
     def test_multiple_users_can_start_lists_at_different_urls(self):
-        # Edith start a new todo list
+        # Edith starts a new to-do list
         self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
